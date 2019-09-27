db1d2028dc36fa46e121613a11aa3fc553fd05ba
Damiano Ciarla <damiano.ciarla@gmail.com>
2019-09-27 16:17:05 +0200

    Fix bundle implementation

--- src/AAAGuardAuthenticatorBundle.php
+++ src/AAAGuardAuthenticatorBundle.php
@@ -3,12 +3,7 @@
 namespace VerisureLab\Library\AAAGuardAuthenticator;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use VerisureLab\Library\AAAGuardAuthenticator\DependencyInjection\AAAGuardAuthenticatorExtension;
 
 class AAAGuardAuthenticatorBundle extends Bundle
 {
-    public function getContainerExtension(): AAAGuardAuthenticatorExtension
-    {
-        return new AAAGuardAuthenticatorExtension();
-    }
 }
--- src/DependencyInjection/AAAGuardAuthenticatorExtension.php
+++ src/DependencyInjection/AAAGuardAuthenticatorExtension.php
@@ -4,12 +4,12 @@ namespace VerisureLab\Library\AAAGuardAuthenticator\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class AAAGuardAuthenticatorExtension extends ConfigurableExtension
+class AAAGuardAuthenticatorExtension extends Extension
 {
-    protected function loadInternal(array $mergedConfig, ContainerBuilder $container): void
+    public function load(array $config, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
